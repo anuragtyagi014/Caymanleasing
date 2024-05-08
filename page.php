08c69b98e198f1756469fe4ea4ab1d0b12314b94
Anurag <anuragtyagi014@gmail.com>
2024-05-08 17:23:56 +0530

    Fixed Bug

--- page.php
+++ page.php
@@ -9,12 +9,13 @@ get_header(); ?>
 <div id="primary" class="site-content">
 	<div id="content" role="main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'content', 'page' ); ?>
-			<?php comments_template( '', true ); ?>
+		<?php while (have_posts()) : the_post(); ?>
+			<?php get_template_part('content', 'page'); ?>
+			<?php comments_template('', true); ?>
 		<?php endwhile; ?>
 
 	</div>
 </div>
+
 
 <?php get_footer(); ?>
